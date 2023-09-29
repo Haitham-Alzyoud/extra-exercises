5ec1658a2648e1435c7fc1916918a1c529967c45
HaithamAlzyoud <haitham.alzyoud@gmail.com>
2023-09-30 01:57:14 +0300

    I've solved my tasks

---
+++ app.js
@@ -0,0 +1,87 @@
+//Q1: Evaluate each of the following JavaScript expressions and show the value:
+
+// -9 * 3 //-27
+// "value is " + 50  // "value is 50"
+// 17 % 5 //2 
+// 5 % 17 //5 
+// 5 / 10  //0.5
+// (4 == 4) //true 
+// (4 != 5) //true 
+// (7 <= 8) //true 
+// Math.ceil(2.5) - Math.floor(2.5)  // 1
+
+
+//Q2: Read a number (using prompt) and display it using alert:
+
+// const number = prompt("Enter a number:");
+// alert(`You entered: ${number}`);
+
+
+// //Q3: Read in two numbers and display them in ascending order:
+
+// const num1 = parseFloat(prompt("Enter the first number:"));
+// const num2 = parseFloat(prompt("Enter the second number:"));
+
+// if (num1 < num2) {
+//     alert(`In ascending order: ${num1}, ${num2}`);
+// } else {
+//     alert(`In ascending order: ${num2}, ${num1}`);
+// }
+
+// //Q4: Read in two numbers and display the larger:
+
+// const numb1 = parseFloat(prompt("Enter the first number:"));
+// const numb2 = parseFloat(prompt("Enter the second number:"));
+
+// if (numb1 > numb2) {
+//     alert(`The larger number is: ${numb1}`);
+// } else if (numb2 > numb1) {
+//     alert(`The larger number is: ${numb2}`);
+// } else {
+//     alert("Both numbers are equal.");
+// }
+
+// //Q5: Read two numbers and display their sum. What problem did you encounter?
+
+// const numbe1 = parseFloat(prompt("Enter the first number:"));
+// const numbe2 = parseFloat(prompt("Enter the second number:"));
+
+// const sum = numbe1 + numbe2;
+// alert(`The sum is: ${sum}`);
+
+// //Q6: If you have a “num” variable,  Your task is to print:
+
+// const num = 1; 
+
+// switch (num) {
+//     case 1:
+//         console.log("ONE");
+//         break;
+//     case 2:
+//         console.log("TWO");
+//         break;
+//     case 3:
+//         console.log("THREE");
+//         break;
+//     case 4:
+//         console.log("FOUR");
+//         break;
+//     case 5:
+//         console.log("FIVE");
+//         break;
+//     case 6:
+//         console.log("SIX");
+//         break;
+//     case 7:
+//         console.log("SEVEN");
+//         break;
+//     case 8:
+//         console.log("EIGHT");
+//         break;
+//     case 9:
+//         console.log("NINE");
+//         break;
+//     default:
+//         console.log("PLEASE TRY AGAIN");
+//         break;
+// }
